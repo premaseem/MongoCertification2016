4f86c1a08d4a8ec2f90b7e1bc0a62a5e4730ceb6
Aseem Jain <Aseem.Jain@rackspace.com>
2016-01-08 16:37:58 -0600

    iterate dict

--- iterateObject.py
+++ iterateObject.py
@@ -8,3 +8,7 @@ for key in person :
 print 'haha '
 
 
+people = {'name': 'Bob', 'hometown': "Palo Alto", 'favorite_color': 'red'}
+for item in people:
+    if (item == 'favorite_color'):
+        print people[item]
