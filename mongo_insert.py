615fadfef01b56c27c1aaeae0a80ee8fbdb0a79b
Aseem Jain <Aseem.Jain@rackspace.com>
2016-01-08 18:47:36 -0600

    first insert in the codebase

---
+++ mongo_insert.py
@@ -0,0 +1,30 @@
+
+
+import pymongo
+import sys
+
+
+connection = pymongo.MongoClient("mongodb://localhost")
+
+db = connection.test
+users = db.users
+
+doc = {'firstname':'Andrewa', 'lastname':'Erlichson'}
+print doc
+print "about to insert the document"
+
+try:
+    users.insert(doc)
+except Exception as e:
+    print "insert failed:", e
+
+print doc
+print "inserting again"
+
+try:
+    users.insert(doc)
+except Exception as e:
+    print "second insert failed:", e
+
+print doc
+
